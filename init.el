796abad212e127f99a3f6fddc1e1873681ea859c
haipengliu <pliu1981@gmail.com>
2016-07-31 02:04:16 -0700

    add helm-gtags

--- init.el
+++ init.el
@@ -195,5 +195,14 @@
 (add-hook `org-mode-hook ( lambda () (setq truncate-lines nil)))
 
 
+;;; Enable helm-gtags-mode
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode)
 
+;; customize
+(custom-set-variables
+ '(helm-gtags-path-style 'relative)
+ '(helm-gtags-ignore-case t)
+ '(helm-gtags-auto-update t))
 
